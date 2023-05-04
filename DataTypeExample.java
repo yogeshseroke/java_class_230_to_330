df6e842001091bb83d41e2141e62b34bf8a1416b
yogeshseroke <99466928+yogeshseroke@users.noreply.github.com>
2023-05-04 15:25:01 +0530

    Add files via upload

---
+++ DataTypeExample.java
@@ -0,0 +1,89 @@
+package class230_330;
+
+public class DataTypeExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//data type :- (1) primitive (2) non-primitive
+		
+		//(1) primitive :- int,float,char,boolean,byte,double,short,long
+	/*	
+		int a = 123;//123 + 12.23 = 135.23
+		
+		float f = 12.23f;
+		
+		double d = 12.23;
+		
+		char c = 'a';
+		
+		boolean b = true;
+		
+		byte by = 12;
+		
+		short s = 1;
+		
+		long l = 123456789;
+		*/
+//		System.out.println("int a = "+ a);
+//		
+//		System.out.println("float f = "+ f);
+//		
+//		System.out.println("double d = "+ d);
+//		
+//		System.out.println("char c = "+ c);
+//		
+//		System.out.println("boolean b = "+ b);
+//		
+//		System.out.println("byte by = "+ by);
+//		
+//		System.out.println("short s = "+ s);
+//		
+//		System.out.println("long l = "+ l);
+//		
+		
+		
+		
+		//int result = (int)f;
+		//float result = f;
+		//int result = (int)(a + f);
+	//float result =  a + f;
+	//System.out.println(result);
+		
+//			int a = 10;
+//			int b = 20;
+//			int result = a + b;
+//			
+//			System.out.println(result);
+//		
+//			int a = 5;
+//			int b = 3;
+//			int result = a%b;
+//			System.out.println(result);
+	
+		//si ka program... result=(p*r*t)/100
+//		float p = 450800.50f;
+//		float r = 2.5f;
+//		float t = 1.6f;
+//		
+//		//int result = (int)((p*r*t)/100);
+//		double result = (p*r*t)/100;
+//		System.out.println(result);
+//		
+		
+		int a = 10;//a=20
+		int b = 20;//b=10
+		System.out.println("a = "+a +" b = "+ b);
+		int c;
+		c = a;//c=10
+		a = b;//a=20
+		b = c;//b=10
+		
+		System.out.println("a = "+a +" b = "+ b);
+		
+		
+		
+		
+	}
+
+}
