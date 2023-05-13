45b20677642638ca781dcb777f8e1b98f809db70
yogeshseroke <99466928+yogeshseroke@users.noreply.github.com>
2023-05-13 17:56:25 +0530

    Add files via upload

---
+++ InputFromUserUsingScannerClassExample.java
@@ -0,0 +1,85 @@
+package class05_06pm;
+
+import java.util.Scanner;
+
+public class InputFromUserUsingScannerClassExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+
+		Scanner sc = new Scanner(System.in);
+		
+//		int b = sc.nextInt();
+//		System.out.println(b);
+//		
+//		System.out.println("enter string value");		
+//		String s = sc.next();
+//		System.out.println(s);
+//		
+//		System.out.println("enter string value as paragraph...");
+//		String s1 = sc.nextLine();
+//		System.out.println(s1);
+		
+
+//		System.out.println("enter string value as paragraph...");
+//		String s1 = sc.nextLine();
+//		System.out.println(s1);
+//		
+//		
+//		System.out.println("enter string value");		
+//		String s = sc.next();
+//		System.out.println(s);
+//		
+//
+//
+//		System.out.println("enter number");
+//		int b = sc.nextInt();
+//		System.out.println(b);
+//
+//		System.out.println("enter string value as paragraph...");
+//		String s2 = sc.nextLine();
+////		String s2 = "abc";
+//		System.out.println(s2);
+
+//		System.out.println("enter floating point value...");
+//		float f = sc.nextFloat();
+//		System.out.println(f);
+//		
+//		System.out.println("enter double value...");
+//		double d = sc.nextDouble();
+//		System.out.println(d);
+		
+//		boolean b = sc.nextBoolean();
+//		System.out.println(b);
+		
+//		char c = sc.next().charAt(0);
+//		System.out.println(c);
+
+		
+		//=====================
+//		System.out.println("enter first number");
+//		int a = sc.nextInt();
+//		System.out.println("enter second number");
+//		int b = sc.nextInt();
+//		int c = a + b;
+//		System.out.println(c);
+		
+		//SI programm
+//		System.out.println("enter p");
+//		int p = sc.nextInt();
+//		System.out.println("enter r");
+//		int r = sc.nextInt();
+//		System.out.println("enter t");
+//		int t = sc.nextInt();
+//
+//		int si = (p*r*t)/100;
+//		
+//		System.out.println("si = "+ si);
+
+		
+	
+	}
+ 
+}
