d1f897fc7171755ecd572fb4df4916f67a1d1473
yogeshseroke <99466928+yogeshseroke@users.noreply.github.com>
2023-05-15 17:56:30 +0530

    Add files via upload

---
+++ SwitchCaseExample.java
@@ -0,0 +1,123 @@
+package class05_06pm;
+
+import java.beans.Statement;
+import java.util.Scanner;
+
+public class SwitchCaseExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+//		switch(option) {
+//		case 1:
+//			statement....
+//			break;
+//			
+//		case 2:
+//			statement....
+//			break;
+//
+//		case 3:
+//			statement....
+//			break;
+//
+//			default :
+//				Statement....
+//				break;
+//		}
+
+	
+//		int a = 4;
+//		
+//		switch(a) {
+//			case 1:
+//				System.out.println("a = 1");
+//				break;
+//			
+//			case 2:
+//				System.out.println("a = 2");
+//				break;
+//			
+//			case 3:
+//				System.out.println("a = 3");
+//				break;
+//				
+//			default :
+//				System.out.println("nothing...");
+//				break;
+//		}
+	
+		
+		//without break....
+		
+//		int a = 2;
+//		switch(a) {
+//		case 1:
+//			System.out.println("a = 1");
+//			
+//		
+//		case 2:
+//			System.out.println("a = 2");
+//			
+//		
+//		case 3:
+//			System.out.println("a = 3");
+//			
+//			
+//		default :
+//			System.out.println("nothing...");
+//			
+//		}
+		
+		//WAP for check whether option match or not....
+		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("choose between F,I,W");
+//		char c = sc.next().charAt(0);
+//		//F-f, i-I, W-w
+//		
+//		switch(c) {
+//			case 'F':
+//			case 'f':
+//				System.out.println("FaceBook...");
+//				break;
+//				
+//			case 'I':
+//			case 'i':
+//				System.out.println("Instagram...");
+//				break;	
+//				
+//			case 'W':
+//			case 'w':
+//				System.out.println("WhatsApp...");
+//				break;	
+//				
+//			default:
+//				System.out.println("please type any of these....F-f, i-I, W-w");
+//				break;
+//		}
+		
+		//even-odd....
+		
+//		int num = 21;
+//		
+//		switch(num%2) {
+//		   case 0:
+//			   System.out.println("Even");
+//			   break;
+//			   
+//		   default :
+//			   System.out.println("Odd");
+//			   break;
+//		}
+		
+		//find any number will be divisible by 3/5/7 or both...
+		//leap year...
+		
+		
+		
+		
+	}
+
+}
