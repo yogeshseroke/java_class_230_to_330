766ca3922174a000c106dcda222e1722ed6a19be
yogeshseroke <99466928+yogeshseroke@users.noreply.github.com>
2023-05-23 18:03:45 +0530

    Add files via upload

--- ArrayExample.java
+++ ArrayExample.java
@@ -19,7 +19,7 @@ public class ArrayExample {
 		/*   Syntax =>
 		 * 	                    1  2  3  4  5    <= length
 					int ar[] = {10,20,30,40,50};
-								0  1  2  3  4    <= index
+								0  1  2  3  4    <= index/size
 								
 								size of array = (length - 1)
 								
@@ -31,6 +31,81 @@ public class ArrayExample {
 			array types :- single dimensional, multidimensional, jagged array 					
 								
 		*/
+		
+		//single dimensional array...
+		
+//		int ar[] = new int[6];
+//		
+//		ar[0] = 90;
+//		ar[1] = 30;
+//		ar[2] = 45;
+//		ar[3] = 9;
+//		ar[4] = 3;
+//		ar[5] = 5;
+		
+//		System.out.println(ar[0]);
+//		System.out.println(ar[1]);
+//		System.out.println(ar[2]);
+		
+//		for(int i=0; i<=5; i++) {
+//			System.out.println(ar[i]);
+//		}
+		
+//		int ar[] = {23,94,85,76};
+//		
+//		for(int i=0; i<=ar.length-1; i++) {
+//			System.out.println(ar[i]);
+//		}		
+		
+		
+		//ascending, descending, maximum, smallest, secondMaximum
+		
+		//ascending and descending
+//		int ar[] = {2,3,9,4,8,5,7,6};
+//		int temp;
+		
+//		for(int i=0; i<=ar.length-1; i++) {
+//			for(int j=i; j<=ar.length-1; j++) {
+//				if(ar[i]>ar[j]) {
+//					temp=ar[i];
+//					ar[i]=ar[j];
+//					ar[j]=temp;
+//				}
+//			}
+//			System.out.print(ar[i]+" ");
+//		}
+		
+		//maximum...
+		
+//		int ar[] = {2,3,9,4,8,5,7,6};
+//		int max = 0;
+//		
+//		for(int i=0; i<=ar.length-1; i++) {
+//		
+//				if(ar[i]>max) {// 2>0|3>2|9>3|4>9|8>9|5>9|7>9|6>9
+//					max=ar[i];//max=2|3|9
+//				}
+//		
+//		}
+//		System.out.println(max);
+		
+//		int ar[] = {2,3,9,4,8,5,7,6};
+//		
+//		int small = ar[0];
+//		
+//		for(int i=0; i<=ar.length-1; i++) {
+//			if(ar[i]<small) {
+//				small=ar[i];
+//			}
+//		}
+//		System.out.println(small);
+		
+		//merge two array...{1,2,3,4,5,6}
+		
+//		int ar1[] = {1,2,3};
+//		int ar2[] = {4,5,6};
+		
+		//add two array...{5,7,9}
 	}
 
 }
